321c848d300dc938f2bb064e84601b2b88c72cb4
KamilaRys <kamila.rys@codewise.com>
2023-12-11 12:25:20 +0100

    Initial commit

---
+++ src/main/java/com/example/HumOrGuess/HumOrGuessApplication.java
@@ -0,0 +1,16 @@
+package com.example.HumOrGuess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class
+})
+public class HumOrGuessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumOrGuessApplication.class, args);
+	}
+
+}
